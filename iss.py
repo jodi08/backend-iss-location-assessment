696222eff5d92939d388bc1e9ee221f57c7e9275
Jo Anna Mollman <joanna.mollman@gmail.com>
2020-07-06 10:33:13 -0700

    Some further setup, layout functions and docstrings

--- iss.py
+++ iss.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
+import requests
+
 
 __author__ = 'Jo Anna Mollman (jodi08 on github)'
+
+def get_current_astronauts():
+    """Connect to API and get astronaut information"""
+    pass
+
+
+def iss_coordinates():
+    """Get current coordinates of ISS and timestamp"""
+    pass
+
+
+def visualize_iss():
+    """make visual to show location of ISS and also list time ISS will next be over Indianapolis, IN"""
+    pass
 
 
 def main():
